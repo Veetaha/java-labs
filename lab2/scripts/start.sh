6b2c58adaf44996ac1bc93363f1bf85a949242e4
veetaha <veetaha2@gmail.com>
2020-05-07 01:19:44 +0300

    get gha token :D

--- lab2/scripts/start.sh
+++
@@ -1,1 +0,0 @@
-java -jar build/libs/lab2-1.0-SNAPSHOT-full.jar
--- lab2/src/main/java/v/e/e/t/a/h/a/dao/IDAOImpl.java
+++
@@ -1,8 +0,0 @@
-package v.e.e.t.a.h.a.dao;
-
-import java.util.List;
-
-public interface IDAOImpl<T> {
-    T getEntity(long id);
-    List<T> getEntityList();
-}
